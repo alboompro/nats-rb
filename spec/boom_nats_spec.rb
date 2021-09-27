20ba7be3ef35bbd312042e2c2cfb965d5f3322b9
Welington Sampaio <welington.sampaio@icloud.com>
2021-09-27 14:58:33 -0300

    :green_heart: add nats services to Github Actions

--- spec/boom_nats_spec.rb
+++ spec/boom_nats_spec.rb
@@ -5,14 +5,18 @@ RSpec.describe BoomNats do
     BoomNats.logger = double("logger", debug: true, info: true, error: true, warn: true)
     allow_any_instance_of(BoomNats::Application).to receive(:wait)
 
-    @s = NatsServerControl.new
+    @s = NatsServerControl.new nats_server
     @s.start_server(true)
     allow_any_instance_of(BoomNats::Application).to receive(:wait)
+    allow_any_instance_of(Kernel).to receive(:exit)
+    allow_any_instance_of(Kernel).to receive(:puts)
   end
 
   after(:each) do
     @s.kill_server
   end
+
+  let(:nats_server) { "nats://#{ENV.fetch("NATS_PORT") { "localhost" }}:#{ENV.fetch("NATS_PORT") { "4222" }}" }
 
   it "has a version number" do
     expect(BoomNats::VERSION).not_to be nil
@@ -25,8 +29,9 @@ RSpec.describe BoomNats do
   end
 
   it "complete test execution" do
+    server = nats_server
     BoomNats.setup do
-      servers "nats://localhost:4222"
+      servers server
       draw_routes do
         in_queue "queue_name" do
           topic "topic_name", A
