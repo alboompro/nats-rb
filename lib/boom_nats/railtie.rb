67581534ab9d010aa9098e67a488c58a4f844edc
Welington Sampaio <welington.sampaio@icloud.com>
2021-10-06 11:53:06 -0300

    :bug: fix zeitwerk eager load all classes

--- lib/boom_nats/railtie.rb
+++ lib/boom_nats/railtie.rb
@@ -1,13 +1,13 @@
 module BoomNats
-  class Railtie < Rails::Railtie
+  class Railtie < ::Rails::Railtie
     initializer "activeservice.autoload", before: :set_autoload_paths do |app|
       # app.config.autoload_paths << ActiveService::Configuration.path
-      app.config.eager_load_paths << Rails.root.join("app", "consumers").to_s
+      app.config.eager_load_paths << ::Rails.root.join("app", "consumers").to_s
     end
 
     initializer "boom_nats.railtie.configure_rails_initialization" do |app|
       app.config.nats = BoomNats.setup
-      BoomNats.logger = Rails.logger
+      BoomNats.logger = ::Rails.logger
     end
 
     server do |app|
